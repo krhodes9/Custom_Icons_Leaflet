7c15e9d8b20e7f88b229a28919b00aafb092d6f3
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-02-21 15:53:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([15.4542, 18.7322], 4);
+var map = L.map('map').setView([10.25, 35.25], 3);
 
-var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
@@ -8,20 +8,19 @@ var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/to
 	ext: 'png'
 }).addTo(map);
 
- var bugIcon = L.icon({
-    iconUrl:'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/480/car-police-front-01-512.png',
-    iconSize: [50,40]
-  });
-  
-$.getJSON("https://services5.arcgis.com/sjP4Ugu5s0dZWLjd/arcgis/rest/services/Swarms_Public/FeatureServer",function(data){
-  var bugIcon = L.icon({
-    iconUrl: '<div>Icons made by <a href="https://cdn2.iconfinder.com/data/icons/spring-outline-6/272/spring-dragonfly-insect_-dragon-lake-fly-bug-512.png',
-    iconSize: [50,40]
-  });
-   
-L.geoJson(data  ,{
-    pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: bugIcon});
-    }
-  }  ).addTo(map);
+  $.getJSON("https://opendata.arcgis.com/datasets/b3f84bff1c514484be7f4d65098f9372_0.geojson",function(data){
+    var bugIcon = L.icon({
+      iconUrl: 'https://pics.freeicons.io/uploads/icons/png/16216416241579606331-512.png',
+      iconSize: [60,50]
+    });
+    var locusts = L.geoJson(data,{
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: bugIcon});
+        marker.bindPopup(feature.properties.STARTDATE + '<br/>' + feature.properties.CAT);
+        return marker;
+      }
+    });
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(locusts);
+    map.addLayer(clusters);
 });
