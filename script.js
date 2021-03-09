108e0b47d2fd6beaca752e3a6ae652f371542138
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-03-08 19:00:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([10.25, 35.25], 8);
+var map = L.map('map').setView([10.25, 35.25], 3);
 
 var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
